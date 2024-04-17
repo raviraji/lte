cd737cc50829628e9082e1666a31d30554b21cb1
raviraji <146082428+raviraji@users.noreply.github.com>
2024-04-17 14:24:05 +0530

    Update test_gpio.py

--- test_gpio.py
+++ test_gpio.py
@@ -1,15 +1,21 @@
-import pulser_gpio as pg 
+import pulser_gpio as pg
+import time
+import sys
 
-import time 
+'''
+1 - for LTE reset
+2 - aeroplane mode
+'''
 
- 
-
-pg.setup(4, 0, 1) 
-
-time.sleep(0.1) 
-
-pg.output(4, 0)  
-
-time.sleep(5) 
-
-pg.output(4,1)
+if int(sys.argv[1]) == 1:
+    pg.setup(4, 0, 1)
+    time.sleep(0.1)
+    pg.output(4, 0) 
+    time.sleep(5)
+    pg.output(4,1)
+elif int(sys.argv[1]) == 2:
+    pg.setup(5, 0, 1)
+    time.sleep(0.1)
+    pg.output(5, 0) 
+    time.sleep(5)
+    pg.output(5,1)
