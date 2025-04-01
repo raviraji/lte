0ddebabffc62f22fde31d0ada5e7e80341d71cae
jira_detect_git <146082428+raviraji@users.noreply.github.com>
2025-04-01 22:04:33 +0530

    Update finaltest.sh

--- finaltest.sh
+++ finaltest.sh
@@ -31,7 +31,7 @@ EOT
 echo ">>>>>>>>>>>>---------------------------------------------------------------------------------<<<<<<<<<<<<<<<<<<<<<"
 file=$(cat /etc/remote-iot/configure | grep name)
 filename=${file:14:4}
-./test.sh>text.txt
+/home/dt/QC/./test.sh>text.txt
 convert -size 2480x3508 -background white -fill black -font Courier label:"$(cat text.txt)" ref.jpg
 echo "---------------------------------------------------script converted to jpg----------------------------------------"
 img2pdf ref.jpg -o $filename.pdf
